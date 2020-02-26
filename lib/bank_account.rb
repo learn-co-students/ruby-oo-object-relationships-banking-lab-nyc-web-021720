201da87831097bc968d1176c60672acad4a94e29
devmullets <devmullets@gmail.com>
2020-02-25 20:22:25 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,44 @@
 class BankAccount
 
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    @@all = [] # array to hold all bank accounts
+
+    def initialize(name)
+        # all accounts open with a balance of "$1k" and a status
+        # of "open". add instance of bank account to class array @@all
+        @name = name
+        @balance = 1000
+        @status = "open"
+        BankAccount.all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def deposit(amount)
+        # deposit #{amount} into current instance of account
+        self.balance += amount
+    end 
+
+    def display_balance
+        # displays current instance balance (self)
+        "Your balance is $#{self.balance}."
+    end 
+
+    def valid?
+        # returns a boolean
+        # (? at end of method name is valid and ? indicates boolean)
+        if self.balance > 0 && self.status == "open"
+            true
+        else
+            false
+        end
+    end 
+
+    def close_account
+        self.status = "closed"
+    end 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,54 @@
+require 'pry'
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  @@all = [] # class variable (array) contains all transfers
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+
+    Transfer.all << self
+  end 
+
+  def self.all
+    @@all
+  end 
+
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    end 
+  end 
+
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      receiver.balance += amount
+      sender.balance -= amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end 
+
+  def reverse_transfer
+    # binding.pry
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else 
+      reject_transfer
+    end 
+  end 
+
 end
