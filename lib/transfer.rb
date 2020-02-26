e3e9defd58f9081923dcfa8c530ddcd86afd1da0
Uriel <u.e.rodriguez@gmail.com>
2020-02-26 12:08:47 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,49 @@
+# Transfers start out in a "pending" status. They can be executed and go to a "complete" state. They can also go to a "rejected" status. A completed transfer can also be reversed and go into a "reversed" status
+
+
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  @@all = []
+
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver 
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def self.all 
+    @@all 
+  end 
+  
+
+  def valid?
+    sender.valid? && receiver.valid? ? true : false 
+  end
+
+  def execute_transaction 
+
+    unless self.class.all.include?(self)
+      if valid? && (self.sender.balance - self.amount) >= 0
+        self.receiver.balance += self.amount 
+        self.status = 'complete'
+        self.class.all << self 
+        self.sender.balance -= self.amount 
+      else 
+        self.status = 'rejected'
+        'Transaction rejected. Please check your account balance.'
+      end
+    end
+  end
+
+  def reverse_transfer
+    if self.class.all.include?(self)
+      self.sender.balance +=  self.amount
+      self.receiver.balance -= self.amount 
+      self.status = 'reversed'
+    end
+  end
+
 end
